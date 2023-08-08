fe70726d9e357b489d4c8b60b3439a0bb99ce724
bodemaxim <bodemaxim@gmail.com>
2023-08-08 09:31:12 +0300

    first commit

---
+++ 20_names-list-2/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NamesList.Context;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers(); //для swagger но скорее всего не понадобится
+builder.Services.AddEndpointsApiExplorer(); //для swagger
+builder.Services.AddSwaggerGen(); //для swagger
+
+//Register ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+
+//if для swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Names}/{action=Index}/{id?}");
+
+app.Run();
